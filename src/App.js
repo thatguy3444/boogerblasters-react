a92a12d25903eceeb4f34b92d48ccaa857d05374
Theodore <theodore.smith@wucapitalgroup.com>
2017-04-27 00:51:59 -0400

    Ejected react project and added modules to css webpack

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.css';
 import AwesomeTest from './awesomeTest';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="AppHeader">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={style.App}>
+        <div className={style.AppHeader}>
+          <img src={logo} className={style.AppLogo} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className={style.AppIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <AwesomeTest />
